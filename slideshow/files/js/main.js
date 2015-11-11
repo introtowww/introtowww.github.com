b64093ea146b5c10d08a63a9130086862746b685
introtowww <ryjohn@macterm00.falabs.rutgers.edu>
2015-11-11 18:10:13 -0500

    Slideshow Lesson

---
+++ slideshow/files/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+	$('.flexslider').flexslider({
+		animation: 'fade'
+	});
+
+	$('.flexslider2').flexslider({
+		animation: 'slide',
+		controlNav: 'thumbnails'
+	});
+
+});
